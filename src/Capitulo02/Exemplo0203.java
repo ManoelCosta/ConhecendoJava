d84fc39d555d495b3543d393eae6dd85b9ba801b
ManoelCosta <ManoelCosta>
2019-03-22 09:36:12 -0300

    Ensino e didática

---
+++ src/Capitulo02/Exemplo0203.java
@@ -0,0 +1,16 @@
+package Capitulo02;
+
+public class Exemplo0203 {
+    public static void main (String args[]){
+        // declaração e inicialização de variáveis
+        int x = 10;     int y = 3;
+        // diversas operações com as variáveis
+        System.out.println("X = "+x);
+        System.out.println("Y = "+y);
+        System.out.println("-X = "+(-x));
+        System.out.println("X/Y ="+(x/y));
+        System.out.println("Resto de X por Y = "+(x%y));
+        System.out.println("Inteiro de X por Y = "+(int)(x/y));
+        System.out.println("X + 1 = "+(++x));
+    }
+}
