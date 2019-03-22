2daaab0e79e6abda5cf9ea5f63e5d092ea445857
ManoelCosta <ManoelCosta>
2019-03-22 12:59:46 -0300

    Esse eu não consegui porque precisa passar paramentros e eu não sei fazer isso pelo intellij e eu não tenho permissão pra usar o prompt dos pcs da unama

---
+++ src/Capitulo02/Exemplo0204.java
@@ -0,0 +1,8 @@
+package Capitulo02;
+
+public class Exemplo0204 {
+    public static void main (String args[]){
+        System.out.println(args[0]);//imprime o 1° argumento na tela
+        System.out.println(args[1]);//imprime o 2° argumento na tela
+    }
+}
